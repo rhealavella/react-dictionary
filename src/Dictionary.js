7203ee834cde091de042214a502591e28115a6fd
rhealavella <rmdeosaran@gmail.com>
2023-09-04 21:06:57 -0400

    Integrated Dictionary API and axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState();
-  function search(event) {
-    event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
   function handleKeyword(event) {
     console.log(event.target.value);
     setKeyword(event.target.value);
   }
+  function search(event) {
+    event.preventDefault();
+
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(url).then(handleResponse);
+  }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
